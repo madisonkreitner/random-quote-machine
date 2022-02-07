5008f2362851dfe998512347ebf5ea24e7c31187
mbradley-github <76399993+mbradley-github@users.noreply.github.com>
2022-02-06 19:52:23 -0700

    Implement random color and quote on page load

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import { CssBaseline, Grid } from '@mui/material';
-import QuoteCard from './QuoteCard'
+import QuoteContainer from './QuoteContainer';
 
 import './App.css'
 
 const App = () => {
     return (
-        <>
-            <CssBaseline />
-            <Grid 
-                container 
-                spacing={3}
-                sx = {{
-                    width: '100%',
-                    height: '100vh',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    bgcolor: '#2596be',
-                    p:0,
-                    margin:0
-                }}
-            >
-                <QuoteCard id="quote-box"/>
-            </Grid>
-        </>
+        <QuoteContainer />
     );
 }
 
